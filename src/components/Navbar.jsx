27af5f005ba1883cc29290ffb21b8f2ae264179d
Krishnendu Sengupta <k.sengupta.29.11@gmail.com>
2021-11-17 13:52:53 +0530

    added title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { Box, Flex, Spacer } from '@chakra-ui/react'
+import { Box, Flex, Spacer, Text } from '@chakra-ui/react'
 import WalletSpace from './WalletSpace'
 
 const Navbar = () => {
     return (
         <Flex p="3" alignItems="center" minH="16">
             <Box alignItems="center">
-                <div>Logo</div>
+                <Text fontSize="xl">ELYS-BTC Bridge</Text>
             </Box>
             <Spacer />
             <WalletSpace />
